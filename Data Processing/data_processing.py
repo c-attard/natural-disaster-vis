06ca8395083674a9387d237798b67bfd85b349e7
c-attard <catherineattard99@gmail.com>
2025-11-20 09:02:13 +1100

    Updated temperature processing

--- Data Processing/data_processing.py
+++ Data Processing/data_processing.py
@@ -2,5 +2,6 @@ import processing_code.disaster_fuzzy_processing
 import processing_code.disaster_preprocessing
 import processing_code.GDP_processing
 import processing_code.location_preprocessing
-import processing_code.temperature_processing
+import processing_code.temperature_processing as temp_proc
 
+temp_proc.process_temps("raw_datasets/HadCRUT.5.0.2.0.analysis.summary_series.global.monthly.csv", "processed_datasets/temperature_data.xlsx")
--- Data Processing/processing_code/temperature_processing.py
+++ Data Processing/processing_code/temperature_processing.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
-temps = pd.read_csv("datasets/HadCRUT.5.0.2.0.analysis.summary_series.global.monthly.csv")
+def process_temps(raw_file, output_file):
 
-temps = temps[['Time', 'Anomaly (deg C)']] # Remove unneeded columns
-temps = temps[temps["Time"] >= "2013"] # Filter for data from 2013 and later
-idx = pd.DatetimeIndex(temps["Time"])
-temps = temps.set_index(idx)
-temp_series = temps["Anomaly (deg C)"]
-temp_series = temp_series.resample('D').interpolate() # Upsample to daily frequency
-temps = temp_series.to_frame()
+    temps = pd.read_csv(raw_file)
 
-temps.to_excel("temperature_data.xlsx")
+    temps = temps[['Time', 'Anomaly (deg C)']] # Remove unneeded columns
+    temps = temps[temps["Time"] >= "2013"] # Filter for data from 2013 and later
+    idx = pd.DatetimeIndex(temps["Time"])
+    temps = temps.set_index(idx)
+    temp_series = temps["Anomaly (deg C)"]
+    temp_series = temp_series.resample('D').interpolate() # Upsample to daily frequency
+    temps = temp_series.to_frame()
+
+    temps.to_excel(output_file)

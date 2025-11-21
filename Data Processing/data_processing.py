d797ec49d793975840b68ef1c2b3462ebd6a5775
c-attard <catherineattard99@gmail.com>
2025-11-21 17:46:58 +1100

    Updated location processing

--- Data Processing/data_processing.py
+++ Data Processing/data_processing.py
@@ -1,9 +1,15 @@
 # import processing_code.disaster_fuzzy_processing
 # import processing_code.disaster_preprocessing
 import processing_code.GDP_processing as gdp_proc
-# import processing_code.location_preprocessing
+import processing_code.location_preprocessing as loc_proc
 import processing_code.temperature_processing as temp_proc
 
-temp_proc.process_temps("raw_datasets/HadCRUT.5.0.2.0.analysis.summary_series.global.monthly.csv",
-                        "final_datasets/temperature_data.xlsx")
+# Process global temperature data
+temp_proc.process_temps("raw_datasets/HadCRUT.5.0.2.0.analysis.summary_series.global.monthly.csv","final_datasets/temperature_data.xlsx")
+
+# Process disaster data
+loc_proc.process_locations("raw_datasets/cities.csv", "raw_datasets/states.csv","raw_datasets/countries.csv","intermediate_datasets/location_data.xlsx")
+
+
+# Process GDP data
 gdp_proc.process_gdp("raw_datasets/mpd2023_web.xlsx", "final_datasets/gdp_data.xlsx")
--- Data Processing/processing_code/location_preprocessing.py
+++ Data Processing/processing_code/location_preprocessing.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
-city_locations = pd.read_csv("datasets/cities.csv")
-state_locations = pd.read_csv("datasets/states.csv")
-country_locations = pd.read_csv("datasets/countries.csv")
+def process_locations(city_file, state_file, country_file, output_file):
 
-city_locations = city_locations[["name", "country_name", "latitude", "longitude"]]
-state_locations =state_locations[["name", "country_name", "latitude", "longitude"]]
-country_locations = country_locations[["name", "latitude", "longitude"]]
+    city_locations = pd.read_csv(city_file)
+    state_locations = pd.read_csv(state_file)
+    country_locations = pd.read_csv(country_file)
+
+    city_locations = city_locations[["name", "country_name", "latitude", "longitude"]]
+    state_locations =state_locations[["name", "country_name", "latitude", "longitude"]]
+    country_locations = country_locations[["name", "latitude", "longitude"]]
 
 
-with pd.ExcelWriter("preprocessed_locations.xlsx") as writer:
-    city_locations.to_excel(writer, sheet_name = 'cities', index = False)
-    state_locations.to_excel(writer, sheet_name = 'states', index = False)
-    country_locations.to_excel(writer, sheet_name = 'countries', index = False)
+    with pd.ExcelWriter(output_file) as writer:
+        city_locations.to_excel(writer, sheet_name = 'cities', index = False)
+        state_locations.to_excel(writer, sheet_name = 'states', index = False)
+        country_locations.to_excel(writer, sheet_name = 'countries', index = False)

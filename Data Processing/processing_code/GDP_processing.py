464edad35357cbc1a548f02818653dff4a11ab81
c-attard <catherineattard99@gmail.com>
2025-11-21 16:16:14 +1100

    Updated GDP processing

--- Data Processing/processing_code/GDP_processing.py
+++ Data Processing/processing_code/GDP_processing.py
@@ -1,43 +1,42 @@
 import pandas as pd
 
-gdps = pd.read_excel("datasets/mpd2023_web.xlsx", sheet_name = "Full data")
-gdps = gdps.drop(["region"], axis = 1) # Remove unneeded columns
-gdps = gdps[gdps["year"] >= 2013] # Filter for 2013 and later
-gdps["month"] = 1
-gdps["day"] = 1
-gdps["date"] = pd.to_datetime(gdps[["year", "month", "day"]])
+def process_gdp(raw_file, output_file):
+
+    gdps = pd.read_excel(raw_file, sheet_name = "Full data")
+    gdps = gdps.drop(["region"], axis = 1) # Remove unneeded columns
+    gdps = gdps[gdps["year"] >= 2013] # Filter for 2013 and later
+    gdps["month"] = 1
+    gdps["day"] = 1
+    gdps["date"] = pd.to_datetime(gdps[["year", "month", "day"]])
 
 
-countries = gdps["country"].unique()
+    countries = gdps["country"].unique()
 
-resampled_gdps = pd.DataFrame()
+    resampled_gdps = pd.DataFrame()
 
-for country in countries:
-    country_data = gdps[gdps["country"] == country]
-    # print(country_data)
+    for country in countries:
+        country_data = gdps[gdps["country"] == country]
 
-    idx = pd.DatetimeIndex(country_data["date"])
-    country_data = country_data.set_index(idx)
+        idx = pd.DatetimeIndex(country_data["date"])
+        country_data = country_data.set_index(idx)
 
-    gdp_series = country_data["gdppc"]
-    pop_series = country_data["pop"]
+        gdp_series = country_data["gdppc"]
+        pop_series = country_data["pop"]
 
-    gdp_series = gdp_series.resample('D').interpolate()
-    pop_series = pop_series.resample('D').interpolate()
+        gdp_series = gdp_series.resample('D').interpolate()
+        pop_series = pop_series.resample('D').interpolate()
 
-    gdp_dict = {"gdppc": gdp_series, "pop": pop_series}
+        gdp_dict = {"gdppc": gdp_series, "pop": pop_series}
 
-    country_data = pd.DataFrame(gdp_dict)
-    country_data["country"] = country
-    # countrycode = gdps[gdps["country"] == country]["countrycode"]
-    # print(countrycode)
-    country_data["countrycode"] = gdps[gdps["country"] == country]["countrycode"].iloc[0]
-    country_data["date"] = country_data.index
+        country_data = pd.DataFrame(gdp_dict)
+        country_data["country"] = country
+        country_data["countrycode"] = gdps[gdps["country"] == country]["countrycode"].iloc[0]
+        country_data["date"] = country_data.index
 
-    resampled_gdps = pd.concat([resampled_gdps, country_data], ignore_index = True)
+        resampled_gdps = pd.concat([resampled_gdps, country_data], ignore_index = True)
 
-gdps = resampled_gdps
+    gdps = resampled_gdps
 
-gdps["Total GDP"] = gdps["gdppc"] * gdps["pop"] # Add column for total GDP
+    gdps["Total GDP"] = gdps["gdppc"] * gdps["pop"] # Add column for total GDP
 
-gdps.to_excel("GDP_data.xlsx")
+    gdps.to_excel(output_file)

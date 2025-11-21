e47ea2730bc0e774a5530e01445acf539eb011db
c-attard <catherineattard99@gmail.com>
2025-11-21 16:17:07 +1100

    Renamed dataset folder

--- Data Processing/data_processing.py
+++ Data Processing/data_processing.py
@@ -4,5 +4,6 @@ import processing_code.GDP_processing as gdp_proc
 # import processing_code.location_preprocessing
 import processing_code.temperature_processing as temp_proc
 
-temp_proc.process_temps("raw_datasets/HadCRUT.5.0.2.0.analysis.summary_series.global.monthly.csv", "processed_datasets/temperature_data.xlsx")
-gdp_proc.process_gdp("raw_datasets/mpd2023_web.xlsx","processed_datasets/gdp_data.xlsx")
+temp_proc.process_temps("raw_datasets/HadCRUT.5.0.2.0.analysis.summary_series.global.monthly.csv",
+                        "final_datasets/temperature_data.xlsx")
+gdp_proc.process_gdp("raw_datasets/mpd2023_web.xlsx", "final_datasets/gdp_data.xlsx")
